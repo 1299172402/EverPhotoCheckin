ba261ae7919baef7b1865de260f7434aff1de1d2
之雨 <1299172402@qq.com>
2022-05-31 18:43:54 +0800

    update: json.loads

--- EverPhotoCheckin.py
+++ EverPhotoCheckin.py
@@ -1,15 +1,16 @@
 import requests
 import time
 import os
+import json
 
 def main():
     ts = int(time.time()*1000)
     baseurl = 'https://openapi.everphoto.cn/sf/3/v4/PostCheckIn'
-    data = os.environ['data']
+    data = json.loads(os.environ['data'])
     data['_rticket'] = str(ts)
     params = '&'.join(f'{k}={v}' for k, v in data.items())
     url = baseurl + '?' + params
-    headers = os.environ['headers']
+    headers = json.loads(os.environ['headers'])
     res = requests.post(url, headers=headers, data='{}')
     print(res.text)
 
