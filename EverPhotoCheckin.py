c97aa47922f7131387273b43dfa0c29518e3a81e
之雨 <1299172402@qq.com>
2022-10-03 18:18:47 +0800

    update
    - 添加每日任务
    - 更新文档为中文

--- EverPhotoCheckin.py
+++ EverPhotoCheckin.py
@@ -1,18 +1,49 @@
-import requests
-import json
 import os
+import time
+import requests
 
-def checkin(token):
-    url = 'https://openapi.everphoto.cn/sf/3/v4/PostCheckIn'
-    headers = {
-        "user-agent": "EverPhoto/4.5.0",
-        "content-type": "application/json",
-        "authorization": f"Bearer {token}",
-    }
-    res = json.loads(requests.post(url, headers=headers).text)
+token = os.environ['token']
+headers = {
+    "content-type": "application/json",
+    "authorization": f"Bearer {token}",
+    "user-agent": "EverPhoto/5.3.0",
+    # "application": "tc.everphoto",
+    # "host": "openapi.everphoto.cn",
+}
+
+# 签到
+CHECKIN_URL = "https://openapi.everphoto.cn/sf/3/v4/PostCheckIn"
+res = requests.post(CHECKIN_URL, headers=headers).json()
+print('[签到状态]')
+print(res)
+
+# 签到报错
+if res['data']['checkin_result']==False:
+    raise Exception('CHECKIN ERROR')
+
+# 延时
+time.sleep(5)
+
+# 任务
+task = {'star', 'hide', 'add_to_album', 'remark'}
+
+for mission_id in task:
+    # 任务（去完成）
+    MISSION_URL = "https://openapi.everphoto.cn/sf/3/v4/MissionReport"
+    res = requests.post(MISSION_URL, headers=headers, json={"mission_id": mission_id}).json()
+    print(f'[任务 {mission_id} 去完成]')
     print(res)
-    if res['data']['checkin_result']!=True:
-        raise Exception('Check-in ERROR')
+    if res['code']!=0:
+        raise Exception('MISSION ERROR')
+    # 延时
+    time.sleep(5)
 
-if __name__ == '__main__':
-    checkin(os.environ['token'])
+    # 任务（领取奖励）
+    REWARD_URL = "https://openapi.everphoto.cn/sf/3/v4/MissionRewardClaim"
+    res = requests.post(REWARD_URL, headers=headers, json={"mission_id": mission_id}).json()
+    print(f'[任务 {mission_id} 领取奖励]')
+    print(res)
+    if res['code']!=0:
+        raise Exception('REWARD ERROR')
+    # 延时
+    time.sleep(5)
--- README.md
+++ README.md
@@ -1,34 +1,34 @@
 # EverPhoto Checkin
 
-Use Github Action to check-in EverPhoto everyday.
+使用 Github Action 完成时光相册的每日签到和每日任务。
 
 ## Secrets
 
 | KEY     | Value                                                        |
 | ------- | ------------------------------------------------------------ |
-| TOKEN    | The `Bearer` in the `authorization`. |
+| TOKEN    | 在 `authorization` 项中的 `Bearer` 值 |
 
-## How to get token
+## 如何获取 token
 
-### The best way
+### 方法一（推荐）
 
-1. Login https://www.everphoto.cn
-2. Press F12
-3. Get it as explained in the picture below
+1. 登录 https://www.everphoto.cn
+2. 按下 F12
+3. 如下图所示获取
    Application => Cookies => access_token
 
 ![image](https://user-images.githubusercontent.com/29673994/172185521-ec8af7d7-61a0-4a90-84a5-8f19fdfcb0be.png)
 
-### Other way
+### 方法二
 
-1. Fork this repository
-2. Go to Action => GetEverPhotoToken => Run workflow
-3. Enter your username and password
+1. Fork 本仓库
+2. 以此点击 Action => GetEverPhotoToken => Run workflow
+3. 输入用户名和密码
 
 ![image](https://user-images.githubusercontent.com/29673994/172188331-7459c9b1-6b0e-4e33-b234-253431df6f9a.png)
 
-4. View the token in the log
-5. **REMOVE the workflow**
+4. 在日志 log 中查看 token
+5. **删除此 workflow （防止他人盗用 token）**
 
 ![image](https://user-images.githubusercontent.com/29673994/172188063-a38588c2-b415-40a1-a2ad-8f996a92e6d0.png)
 
